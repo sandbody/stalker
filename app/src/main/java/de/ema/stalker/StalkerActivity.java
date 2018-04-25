e15c3292ad752ea614abdb59c661e73002bd677f
develkd <develkd@gmail.com>
2018-04-25 23:37:54 +0200

    Initial commit

---
+++ app/src/main/java/de/ema/stalker/StalkerActivity.java
@@ -0,0 +1,13 @@
+package de.ema.stalker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StalkerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stalker);
+    }
+}
